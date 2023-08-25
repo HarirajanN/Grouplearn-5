1748dadfdd2581ce8fbecefa245b8273fa802742
Harirajan N <139871782+HarirajanN@users.noreply.github.com>
2023-08-25 15:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var loadFile = function (event) {
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+
+
+
+
+
+
+  let Name  = promp('Enter your name')
+document.getElementById('name').innerHTML = Name
+
+let Country = promp('Enter Your Country')
+document.getElementById('country').innerHTML = Country
+ 
+//let Following = promp('Enter Your Following '+"(◕‿◕)")
+//document.getElementById('count').innerHTML = Following
+
+//India
